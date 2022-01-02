b56c7c5647bf5abd139513de8119b2a636a33f2a
Chiraag Juvekar <chiraag.juvekar@gmail.com>
2022-01-02 15:42:22 -0500

    Initial database migration with alembic

---
+++ crew_game/backend/database/database.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///crew_backend.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@as_declarative()
+class Base:
+    id: Any
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
